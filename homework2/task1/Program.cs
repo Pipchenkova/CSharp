5be5dd6ffb0b412fccd44d97fbc90ef2b9b8b90a
Olga <pipchenkova@mail.ru>
2022-09-20 18:54:59 +0300

    task2

---
+++ homework2/task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Загадано число: ");
+int N = new Random().Next(100,1000);
+Console.WriteLine(N);
+int a = N/10;
+int number = a%10;
+Console.WriteLine($"Вторая цифра числа - {number}");
+
+
