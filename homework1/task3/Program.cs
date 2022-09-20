9b8aa2d014621805d099a6b3efa961769226f238
Olga <pipchenkova@mail.ru>
2022-09-20 15:07:16 +0300

    task3

---
+++ homework1/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Загадано число: ");
+//int a = new Random().Next();
+int a = -5;
+Console.WriteLine(a);
+if (a%2 == 0){
+   Console.WriteLine("Число является четным"); 
+}
+else{
+     Console.WriteLine("Число является нечетным"); 
+}
