089e38f66ca3ca4d15cdf32c14d988496c86b86b
Olga <pipchenkova@mail.ru>
2022-09-20 14:53:05 +0300

    add task2

---
+++ homework1/task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Даны 3 числа: ");
+int a = 9;
+int b = 8;
+int c = 6;
+int max = a;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+if (b>max){
+    max = b;
+}
+if(c>max){
+    max=c;
+
+}
+Console.WriteLine($"Максимальное числo: {max} ");
+
