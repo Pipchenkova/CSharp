a8fd2cec7551416fc58c9f2137e74b07e1d4c060
Olga <pipchenkova@mail.ru>
2022-09-20 13:53:08 +0300

    изменила ввод данных пользователем

--- homework1/task1/Program.cs
+++ homework1/task1/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введтите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введтите второе число:");
+int b =Convert.ToInt32(Console.ReadLine());
+if (a > b){
+    Console.Write("наибольшее число ");
+    Console.WriteLine(a);
+    Console.Write("наименьшее число ");
+    Console.WriteLine(b);
+
+}
+else{
+    Console.Write("наибольшее число ");
+    Console.WriteLine(b);
+    Console.Write("наименьшее число ");
+    Console.WriteLine(a);
+
+}
+
