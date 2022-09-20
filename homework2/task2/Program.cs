ce0a657f8334267a6f05f3b3cf3392651febc44a
Olga <pipchenkova@mail.ru>
2022-09-20 22:04:08 +0300

    task2

---
+++ homework2/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Задано число: ");
+ //int N = new Random().Next();
+ int N = 0;
+ Console.WriteLine(N);
+ if(N/100==0){
+    Console.WriteLine("В числе менее трех цифр");
+ }
+else{
+    int count = 0;
+    int a = N;
+    while(Math.Abs(a)/10>0){
+        count ++;
+        a = a/10;
+    }
+    int number = Math.Abs(N)/Convert.ToInt32(Math.Pow(10,(count - 2)));
+    int number1 = number%10;
+    Console.WriteLine($"Третья цифра числа - {number1}");
+}
