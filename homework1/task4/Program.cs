d28e48b0adbc380cebc56ea0298dfada7ca1655c
Olga <pipchenkova@mail.ru>
2022-09-20 16:52:45 +0300

    task4

---
+++ homework1/task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Дано число: ");
+int N = new Random().Next(1,21);
+//int N = 1;
+Console.WriteLine(N);
+int start = 2;
+if( N>1){
+Console.WriteLine("Четные числа, которые в него входят: ");
+while(start<=N){
+    Console.WriteLine(start);
+    start +=2;
+}
+}
+else{
+   Console.WriteLine(" Четных чисел нет");
+}
+
