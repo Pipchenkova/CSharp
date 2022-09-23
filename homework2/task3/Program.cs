40644d448c2ea492e95c06ff3239846824c317c8
Olga <pipchenkova@mail.ru>
2022-09-23 20:47:54 +0300

    task3

---
+++ homework2/task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Сегодня день: ");
+int N = new Random().Next(1,8);
+Console.WriteLine(N);
+if (N>5)
+{
+  Console.WriteLine("Ура! Выходной"); 
+}
+else{
+    Console.WriteLine(" Иди работай");
+}
+
+
+
