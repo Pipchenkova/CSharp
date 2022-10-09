85ba60467b9e052fd40bc47750a5395598a462d6
Olga <pipchenkova@mail.ru>
2022-10-09 22:41:50 +0300

    task3

---
+++ homework3/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 23
+
+//Напишите метод, который принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+double[] Cub(int number)
+{
+    double[] res1 = new double[number];
+    for (int i = 0; i < number; i++)
+    {
+        res1[i] = Math.Pow(i + 1, 3);
+    }
+    return res1;
+}
+
+Console.WriteLine(String.Join(",", Cub(5)));
+
