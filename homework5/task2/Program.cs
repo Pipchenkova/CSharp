9cfce4b738aeaab2c2febdb2e6c24fb5b5f23f5a
Olga <pipchenkova@mail.ru>
2022-10-20 17:08:42 +0300

    добавила метод во вторую задачу

--- homework5/task2/Program.cs
+++ homework5/task2/Program.cs
@@ -2,22 +2,61 @@
 //Найдите сумму элементов, стоящих на нечётных позициях
 
 // 1.получаем случайное число - размер массива
-// 2. заполняем случайными числами
-// 3. печатаем массив
-// 4. берем позиции через один. начиная с i=0, и складываем значения
+//2. создаем массив
+// 3. заполняем случайными числами
+// 4. печатаем массив
+// 5. берем позиции через один. начиная с i=1, и складываем значения
 
-int size = new Random().Next(2,10);
-int[] array = new int[size];
-for( int i=0; i<size; i++)
+// int size = new Random().Next(2,10);
+// int[] array = new int[size];
+// for( int i=0; i<size; i++)
+// {
+//     array[i] = new Random().Next(-10,10);
+// }
+// Console.WriteLine(string.Join(",",array));
+// int sum = 0;
+// for( int i=1; i<size; i=i+2)
+// {
+//     sum +=array[i];
+// }
+// Console.WriteLine(sum);
+
+int GetValue(int min, int max)
+      {
+       return new Random().Next(min, max);
+    }
+    int[] CreateArray(int value)
+     {
+     return new int[value];
+    }
+void Fill(int[] Array)
 {
-    array[i] = new Random().Next(-10,10);
+    int size = Array.Length;
+    for (int i = 0; i<size; i++)
+    {
+        Array[i] = GetValue(1, 20);
+    }
 }
-Console.WriteLine(string.Join(",",array));
-int sum = 0;
-for( int i=1; i<size; i=i+2)
+void Print(int[] Array)
 {
-    sum +=array[i];
+    Console.WriteLine("[" + string.Join(",", Array) + "]");
 }
-Console.WriteLine(sum);
+int Final(int[]array)
+{
+    int size = array.Length;
+    int sum = 0;
+    for (int i = 1; i< size; i=i+2)
+    {
+
+     sum +=array[i];
+   }
+   return sum;
+    }
+int size = GetValue(3,7);
+int[] array = CreateArray(size);
+Fill(array);
+Print(array);
+int result = Final(array);
+Console.WriteLine(result);
 
 
